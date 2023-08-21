2e40e6e5f0bc6a990bbc10f39fe00ab88c08db88
shiroshan <siroshang@gmail.com>
2023-08-21 14:03:30 +0530

    feat: Add prisma service

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
 import { ConfigModule } from '@nestjs/config';
+import { PrismaModule } from './prisma/prisma.module';
 
 @Module({
   imports: [
@@ -10,6 +11,7 @@ import { ConfigModule } from '@nestjs/config';
       isGlobal: true,
     }),
     UsersModule,
+    PrismaModule,
   ],
   controllers: [AppController],
   providers: [AppService],
